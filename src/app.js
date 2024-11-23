ec166ca7c79ebb04161a92f31650d347df584428
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-11-23 16:58:29 -0300

    feat: Add Hotel Routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import companyRouter from "./routes/company.routes.js";
+import hotelRouter from "./routes/hotel.routes.js";
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRouter);
 app.use("/company", companyRouter);
+app.use("/hotel", hotelRouter);
 
 export default app;
