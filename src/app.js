fd675bad21041ab2a50bd5d0a962e3a3a681ba46
harrison <d.harrison.lima@gmail.com>
2024-11-23 14:22:52 -0300

    chore: init config

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export default app;
