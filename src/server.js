0c9fe3dde3fcb1348e1621b76f9bbcea45c37c05
harrison <d.harrison.lima@gmail.com>
2024-11-23 15:15:22 -0300

    feat: authenticate use-cases and errors  folder

--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ import app from "./app.js";
 import "dotenv/config";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
-const HOST = "0.0.0.0";
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost${PORT}`);
 });
---
+++ src/use-cases/authenticate.js
@@ -0,0 +1,26 @@
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error.js";
+import { compare } from "bcryptjs";
+
+export class AuthenticateUseCase {
+  constructor(usersRepository) {
+    this.usersRepository = usersRepository;
+  }
+
+  async execute({ email, password }) {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      user,
+    };
+  }
+}
