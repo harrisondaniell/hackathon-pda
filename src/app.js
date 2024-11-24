3fd04f53c11af4cb4c2b0a470c604942c5e55b58
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-11-24 14:24:00 -0300

    feat: Add reservation routes

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import companyRouter from "./routes/company.routes.js";
 import hotelRouter from "./routes/hotel.routes.js";
+import reservationRouter from "./routes/reservation.routes.js";
 import { expressjwt } from "express-jwt";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
 };
 app.use(cors(corsOptions));
 
@@ -22,35 +23,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-  expressjwt({
-    secret: env.JWT_SECRET,
-    algorithms: ["HS256"],
-    requestProperty: "auth",
-  }).unless({
-    path: [
-      "/user/login",
-      "/user/register",
-      "/user/token/refresh",
+    expressjwt({
+        secret: env.JWT_SECRET,
+        algorithms: ["HS256"],
+        requestProperty: "auth",
+    }).unless({
+        path: [
+            "/user/login",
+            "/user/register",
+            "/user/token/refresh",
 
-      { url: /^\/public\/.*/, methods: ["GET"] },
-    ],
-  })
+            { url: /^\/public\/.*/, methods: ["GET"] },
+        ],
+    })
 );
 
 app.use((err, req, res, next) => {
-  console.log(req.cookies);
-  if (err.name === "UnauthorizedError") {
-    console.log("Erro de autorização:", err);
-    return res.status(401).json({
-      error: "Não autorizado",
-      details: err.message,
-    });
-  }
-  next(err);
+    console.log(req.cookies);
+    if (err.name === "UnauthorizedError") {
+        console.log("Erro de autorização:", err);
+        return res.status(401).json({
+            error: "Não autorizado",
+            details: err.message,
+        });
+    }
+    next(err);
 });
 
 app.use("/user", userRouter);
 app.use("/company", companyRouter);
 app.use("/hotel", hotelRouter);
+app.use("/reservation", reservationRouter);
 
 export default app;
---
+++ src/routes/reservation.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createReservation, getReservationsByHotelId, getReservationsByUserEmail, deleteReservation, updateReservation } from '../controllers/reservation.controller';
+
+const reservationRouter = express.Router();
+
+reservationRouter.get('/user/:email', getReservationsByUserEmail);
+reservationRouter.get('/hotel/:hotelId', getReservationsByHotelId);
+reservationRouter.post('/', createReservation);
+reservationRouter.patch('/:id', updateReservation);
+reservationRouter.delete('/:id', deleteReservation);
+
+export default reservationRouter;
